f7f0e885dfc3f4075839b7bead00d3bafeb42486
SaraBalbin <51859641+SaraBalbin@users.noreply.github.com>
2025-05-03 10:06:25 -0500

    Refactor en el codigo

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,25 +1,37 @@
-# obtain a list of files in the input directory
 import os
 
-files_in_input_dir=os.listdir('data/input/')
-
-# count the frequency of the words in the files in the input directory
-counter={}
-for filename in files_in_input_dir:
-    with open('data/input/'+filename) as f:
-        for l in f:
-            for w in l.split( ):
-                w = w.lower().strip(",.!?")
-                counter[w] = counter.get(w, 0) + 1
-
-# create the directory output/ if it doesn't exist
-if not os.path.exists('data/output'):
-    os.makedirs('data/output')
-
-# save the results using tsv format
-with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and value to the file
-            f.write(f"{key}\t{value}\n")
+from .write_count_words import write_count_words
 
 
+def main():
+
+    ##
+    input_file_list = os.listdir("data/input/")
+    # all_lines = read_all_lines()
+
+    ## read all lines
+    ## preprocess lines
+    ## split in words
+    ## count words
+    ## write count words
+
+    # count the frequency of the words in the files in the input directory
+    counter = {}
+    for filename in input_file_list:
+        with open("data/input/" + filename) as f:
+            for l in f:
+                for w in l.split():
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+
+    ##
+    # create the directory output/ if it doesn't exist
+    write_count_words(counter)
+
+
+if __name__ == "__main__":
+    main()
+
+    
+    
+    
